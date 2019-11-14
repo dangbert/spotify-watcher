8c519fda33b99abb7a3a4ca1f1667453b1275703
Daniel Engbert <end1@umbc.edu>
2019-11-14 00:55:09 -0500

    Added support for very long playlists. hot_playlist.py now works exactly as desired (except removed songs aren't added to the backup playlist yet).

--- spotify-watcher/hot_playlist.py
+++ spotify-watcher/hot_playlist.py
@@ -27,9 +27,7 @@ def main():
     # (args starting with '--' are made optional)
     parser.add_argument('--backup_uri', type=str, help='(optional string) uri of spotify playlist to also add the songs removed from playlist_uri.')
     args = parser.parse_args()
-    if len(sys.argv)==1:
-        print("example usage:")
-        print('  ./hot_playlist.py dangbert 40 "spotify:playlist:40dcGwdCvUDFwS893qdaLd" --backup_uri "spotify:playlist:40dcGwdCvUDFwS893qdaLd"\n')
+    if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         exit(1)
 
@@ -45,17 +43,15 @@ def main():
     # modify playlist
     hot_playlist(sp, args.username, args.max_days, args.playlist_uri, args.backup_uri)
 
-
 # remove all songs older than max_days from given playlist:
 #   (copy removed songs to backup_uri playlist if not None)
 # based on:
 #  https://github.com/plamere/spotipy/blob/master/examples/remove_specific_tracks_from_playlist.py
 #  https://github.com/plamere/spotipy/blob/master/examples/read_a_playlist.py
 def hot_playlist(sp, username, max_days, playlist_uri, backup_uri=None):
-    # TODO: ensure this works with very long playlists (over 100 songs)
     playlist_id = playlist_uri.split(':')[2]
-    results = sp.user_playlist(username, playlist_id)
-    #print(json.dumps(results, indent=4))
+    results = get_all_playlist_tracks(sp, username, playlist_id)
+    print("playlist length: " + str(len(results['tracks']['items'])))
 
     now = timeNowUTC()
     track_ids = [] # list of tracks to remove
@@ -67,21 +63,43 @@ def hot_playlist(sp, username, max_days, playlist_uri, backup_uri=None):
 
             # get the delta time since playlist was added
             delta = relativedelta(now, addDate)
-            print("age: ({}y, {}m, {}d), {}h:{}m:{}s \t --- #{} '{}'".format(delta.years, delta.months, delta.days, delta.hours, delta.minutes, delta.seconds, index, item["track"]["name"]))
+            elapsedDays = (now - (now-delta)).days # (more magic) https://stackoverflow.com/a/43521106
 
-        # TODO: account for if delta is 1 year, 1 hour (for example)
-        #   https://dateutil.readthedocs.io/en/stable/relativedelta.html
-        #if delta.hours > 1:
-        # TODO: for now just checking dummy condition
-        if dateStr == None or index == 11:
+            print("age: ({}y, {}m, {}d), {}h:{}m:{}s \t== {} days \t --- #{} '{}'".format(delta.years, delta.months, delta.days, delta.hours, delta.minutes, delta.seconds, elapsedDays, index, item["track"]["name"]))
+        else:
+            print("dateStr None for song " + item["track"]["name"])
+
+        if dateStr == None or elapsedDays >= max_days:
             track_ids.append( { "uri" : item["track"]["id"], "positions": [ int(index)] } )
-            #track_ids.append( { "uri" : item["track"]["name"], "positions": [ int(index)] } ) # TODO: for now use name for clarity in output
 
+    print("\n\nsongs to remove:")
     print(json.dumps(track_ids, indent=4))
-    exit(0)
-    print("deleting " + str(len(track_ids)) + " tracks")
-    # remove desired tracks:
-    sp.user_playlist_remove_specific_occurrences_of_tracks(username, playlist_id, track_ids)
+    if len(track_ids) == 0:
+        print("\nNo songs ready to remove!")
+    elif input("\nRemove " + str(len(track_ids)) + " songs from playlist '" + results["name"] + "'? (y/n): " ).lower().strip() in ('y','yes'):
+        # remove desired tracks:
+        sp.user_playlist_remove_specific_occurrences_of_tracks(username, playlist_id, track_ids)
+        print("Done!")
+    else:
+        print("Aborting without modifying playlist.")
+
+    if backup_uri != None:
+        pass
+        # TODO: add songs in track_ids to backup_uri playlist 
+
+# returns the json results for all the tracks in a playlist (even if > 100 songs)
+def get_all_playlist_tracks(sp, username, playlist_id):
+    results = sp.user_playlist(username, playlist_id)
+
+    while results['tracks']['next']:
+        # https://github.com/plamere/spotipy/issues/246#issuecomment-359128303
+        #   note that tmpResults has exact same format as results['tracks']
+        #   ._get is a workaround to issue with .next() being out of date
+        tmpResults = sp._get(results['tracks']['next'])
+        results['tracks']['items'].extend(list(tmpResults['items']))
+        results['tracks']['next'] = tmpResults['next'] # update next value
+    #print(json.dumps(results, indent=4))
+    return results
 
 # returns datetime for the UTC time now (location aware)
 # (can be used for relativedelta calculations)
