b374543b924c8d580fc341eb755f3c96ee892b7e
Daniel Engbert <end1@umbc.edu>
2019-11-17 19:49:54 -0500

    remove_duplicates.py is now done option --keep_oldest is used to select (for each duplicate) whether to keep the song that was most or least recently added to playlist

--- spotify-watcher/remove_duplicates.py
+++ spotify-watcher/remove_duplicates.py
@@ -10,20 +10,30 @@ import pprint
 import argparse
 import sys
 import json
+
+from dateutil import parser
+from dateutil.relativedelta import *
 from datetime import datetime
+import pytz
+
 
 def main():
-    # TODO: add arg that will generate a settings file for all my playlist so I can batch run this on all my playlists
+    # TODO: add arg that will generate a settings file for all my playlist so I can batch run this on all my playlists (in a shell script)
     # parse args
     parser = argparse.ArgumentParser(description='Removes all duplicate songs in the given Spotify playlist. (Prompts confirmation before removal).')
     parser.add_argument('username', type=str, help='(string) username of spotify account owning the playlist.')
     parser.add_argument('playlist_uri', type=str, help='(string) uri of spotify playlist to modify (e.g. "spotify:playlist:i0dcGw2CvUDFwx833UdaLf"')
     # (args starting with '--' are made optional)
-    parser.add_argument('--keep_oldest', type=str, default=True, help='(bool) set "false" to preserve the duplicate that was most recently added to the playlist instead of the oldest')
+    parser.add_argument('--keep_oldest', type=str, default='true', help='(string) set "false" to preserve the duplicate that was most recently added to the playlist instead of the oldest')
     args = parser.parse_args()
     if len(sys.argv) == 1:
         parser.print_help(sys.stderr)
         exit(1)
+    args.keep_oldest = args.keep_oldest.lower()
+    if args.keep_oldest not in ["true", "false"]:
+        print("ERROR: argument for --keep_oldest must be 'true' or 'false'")
+        exit(1)
+    args.keep_oldest = True if args.keep_oldest == "true" else False
 
     # setup API
     scope = 'playlist-modify-public'
@@ -43,60 +53,73 @@ def remove_duplicates(sp, username, playlist_uri, keepOldest=True, verbose=True)
     if verbose:
         print("Running remove_duplicates: " + str(datetime.today()) + "\n")
     playlist_id = playlist_uri.split(':')[2]
-#
     # get entire list of tracks in playlist
     results = get_all_playlist_tracks(sp, username, playlist_id)
     #print(json.dumps(results, indent=4))
 
-    # TODO: use keepOldest (perhaps iterate the list backwards... but make sure index in track_ids is correct for removal)
-    # TODO: need to use the date_added instead of index!!!!
-
-    # search for duplicate songs, and mark the indices of the ones to be removed in deleteMap:
-    deleteMap = {}     # indices of songs in here that are stored as True will all be deleted
+    # search for duplicate songs
+    deleteMap = {}     # maps track ids to list of indices of all duplicate occurences
     for index, item in enumerate(results["tracks"]["items"]):
-        # if we have already labeled this song for keep/deletion then skip it
-        if index in deleteMap:
+        # if we have already labeled this song as a duplicate then continue
+        if item['track']['id'] in deleteMap and index in deleteMap[item['track']['id']]:
             continue
+
         # compare to rest of items in playlist
         for index2, item2 in enumerate(results["tracks"]["items"]):
             if index == index2:
                 continue
-            if are_songs_duplicates(item, item2):
-                deleteMap[index] = False # indicate we know it has a duplicate, but to keep this one
-                deleteMap[index2] = True # mark the "other" one for deletion
+            if item['track']['id'] == item2['track']['id']: # duplicates
+                if not item['track']['id'] in deleteMap:
+                    deleteMap[item['track']['id']] = []
+                arr = deleteMap[item['track']['id']]
+                if not index in arr:
+                    arr.append(index)
+                if not index2 in arr:
+                    arr.append(index2)
 
-    track_ids = [] # populate list of songs to remove
+    # for each set of duplicates, pick the song that is oldest/newest to the playlist to keep (based on keepOldest)
+    deleteList = []  # indices to remove
+    protectList = [] # indices to protect (don't delete even if its in deleteList)
+    for track_id in deleteMap:
+        arr = deleteMap[track_id] # list of indices of duplicate songs of track_id
+        deleteList.extend(arr)
+        # now put one song in protectList (the version of this duplicate to keep)
+        ageList = [0 for x in arr]
+        for i, itemsIndex in enumerate(arr):
+            # convert date to epoch seconds (magic date parser)
+            dateStr = results["tracks"]["items"][itemsIndex]["added_at"] # e.g. "2019-11-12T04:21:15Z"
+            ageList[i] = 0 if dateStr == None else int(parser.parse(dateStr).timestamp())
+        oldest = ageList.index(min(ageList))
+        newest = ageList.index(max(ageList))
+        protectList.append(arr[oldest] if keepOldest else arr[newest])
+
+    remove_list = []  # populate (properly formatted) list of songs to remove
     # also print everything to be removed in order
     for index, item in enumerate(results["tracks"]["items"]):
-        if index in deleteMap and deleteMap[index] == True:
-            track_ids.append( { "uri" : item["track"]["id"], "positions": [index] } )
+        if index in deleteList and not index in protectList:
+            remove_list.append( { "uri" : item["track"]["id"], "positions": [index] } )
             if verbose:
                 print("X\tindex: #{}, name: {}".format(index, item["track"]["name"]))
         elif verbose:
             print("\tindex: #{}, name: {}".format(index, item["track"]["name"]))
 
-    if len(track_ids) == 0:
+    # print info and do the delete with the user's confirmation
+    if len(remove_list) == 0:
         if verbose:
             print("\n--------------\nNo duplicates to remove!")
         return
     if verbose:
         print("\n--------------")
-        print("* after removal " + str(len(results["tracks"]["items"]) - len(track_ids)) + " song(s) will remain in playlist '" + results["name"] + "'")
-    if not verbose or input("\nRemove " + str(len(track_ids)) + " songs from playlist '" + results["name"] + "'? (y/n): " ).lower().strip() in ('y','yes'):
-        runAgain = len(track_ids) > 100
+        print("* after removal " + str(len(results["tracks"]["items"]) - len(remove_list)) + " song(s) will remain in playlist '" + results["name"] + "'")
+    if not verbose or input("\nRemove " + str(len(remove_list)) + " songs from playlist '" + results["name"] + "'? (y/n): " ).lower().strip() in ('y','yes'):
+        runAgain = len(remove_list) > 100
 
-        track_ids=track_ids[:100] # force length <= 100
-        sp.user_playlist_remove_specific_occurrences_of_tracks(username, playlist_id, track_ids)
+        remove_list=remove_list[:100] # force length <= 100
+        sp.user_playlist_remove_specific_occurrences_of_tracks(username, playlist_id, remove_list)
         if runAgain: # recursively continue deleting the rest of the tracks
             remove_duplicates(sp, username, playlist_uri, keepOldest, False)
     else:
         print("Aborting without modifying playlist.")
-
-# Returns true if songs are considered duplicates
-def are_songs_duplicates(s1, s2):
-    if s1['track']['id'] == s2['track']['id']:
-        return True
-    return False
 
 if __name__ == "__main__":
     main()
