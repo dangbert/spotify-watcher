4328e3f11a141cae1f48370d7a1de8562883f38f
Daniel Engbert <end1@umbc.edu>
2019-11-23 21:26:16 +0000

    Fixed bugs with directories for autorun scripts and verfied they work fine now as cron jobs.

--- spotify-watcher/.autorun/autorun-cool_artists.sh
+++ spotify-watcher/.autorun/autorun-cool_artists.sh
@@ -7,10 +7,11 @@
 
 NUM_COPY=5
 
-cd /home/pi/autorun-spotify
-source secret.env
-source playlists.env
+BASE="/home/pi/Downloads/misc-tools/spotify-watcher/.autorun"
+cd "$BASE"
+source "secret.env"
+source "playlists.env"
 
-LOG="./log--cool_artists.txt"
+LOG="log--cool_artists.txt"
 
-yes | ./cool_artists.py dangbert "$COOL_ARTISTS" "$PLAY_COOL_ARTISTS" --copy_num $NUM_COPY --delete_after True >>$LOG 2>&1
+yes | ../cool_artists.py dangbert "$COOL_ARTISTS" "$PLAY_COOL_ARTISTS" --copy_num "$NUM_COPY" --delete_after True >>$LOG 2>&1
